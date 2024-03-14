335783d34736830a95b7d377007190ac7193367b
miakojason <miakojason@gmail.com>
2024-03-14 21:32:17 +0800

    back admin ok

---
+++ front/que.php
@@ -0,0 +1,38 @@
+<fieldset>
+    <legend>目前位置>首頁>問卷調查</legend>
+    <table>
+        <tr>
+            <th>編號</th>
+            <th>問卷題目</th>
+            <th>投票總數</th>
+            <th>結果</th>
+            <th>狀態</th>
+        </tr>
+        <?php
+        $rows = $Que->all(['subject_id' => 0]);
+        foreach ($rows as $idx => $row) {
+        ?>
+            <tr>
+                <td><? $idx + 1; ?></td>
+                <td><?= $row['text']; ?></td>
+                <td><?= $row['vote']; ?></td>
+                <td><a href="?do=result&id=<?= $row['id']; ?>">結果</a></td>
+                <td>
+                    <?php
+                    if (isset($_SESSION['user'])) {
+                    ?>
+                        <a href='?do=vote&id=<?= $row['id']; ?>'>參與投票</a>
+                    <?php
+                    } else {
+                    ?>
+                    <a href='?do=login'>請先登入</a>
+                    <?php
+                    }
+                    ?>
+                </td>
+            </tr>
+        <?php
+        }
+        ?>
+    </table>
+</fieldset>
